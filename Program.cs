3dada7d64e9f21872ad7bd1e6fb16ecdcf1af2eb
Raluca <ralucaandrailie@gmail.com>
2023-11-19 16:07:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tema
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+            Ford.fullThrottle();
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Opel.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            
+            Console.Read();
+        }
+    }
+}
